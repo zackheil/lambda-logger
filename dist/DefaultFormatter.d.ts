6880f3e8ecd889c4275b97a9065a23d01e022737
Zack Heil <zheilb@me.com>
2020-08-10 17:11:56 -0500

    Updated CI/CD. Added npm prepare for builds

--- dist/DefaultFormatter.d.ts
+++
@@ -1,4 +0,0 @@
-import { LogFormatterStructure, LogEvent, Stream } from "./compiler/types";
-export default class LogFormatter implements LogFormatterStructure {
-    format(event: LogEvent, outputStreams: Stream[]): void;
-}
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import Logger from './Logger';
-import { LoggerStructure, LogFormatterStructure, LogLevel, LogMessage, Stream, LogProperties, LogEvent, OutputStream } from './compiler/types';
-export { LoggerStructure, LogFormatterStructure, LogLevel, LogMessage, Stream, LogProperties, LogEvent, OutputStream, };
-export default Logger;
