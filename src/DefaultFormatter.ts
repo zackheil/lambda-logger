2e288783bf293e2efb2a3d3c95ca6eb1a88676f3
Zack Heil <zheilb@me.com>
2020-08-13 17:07:30 -0500

    Fixed a compilation error due to legacy DefaultFormatter code

--- src/DefaultFormatter.ts
+++ src/DefaultFormatter.ts
@@ -8,19 +8,19 @@ export default class LogFormatter implements LogFormatterStructure {
 
             // Output the log event info to a stream
             output.write(`[${event.timestamp} - ${LogLevel[event.level].toUpperCase()}]: ${format(event.message.formatString, ...event.message.args)}\n`);
-            if (event.firstFive && event.level > LogLevel.info) {
-                output.write(`\t${LogLevel[event.level].toUpperCase()}: Printing the first and last 5 log message to aid in issue reproduction:\n`);
-                event.firstFive.forEach(msg => {
-                    output.write(`\tLOG #${msg.logCount}: [${LogLevel[msg.level].toUpperCase()}]: ${format(msg.message.formatString, ...msg.message.args)}\n`);
-                });
-            }
-            if (event.lastFive && event.level > LogLevel.info) {
-                if (event.logCount > 11)
-                    output.write(`\t[... ${event.logCount - 11} more messages ...]\n`);
-                event.lastFive.forEach(msg => {
-                    output.write(`\tLOG #${msg.logCount}: [${LogLevel[msg.level].toUpperCase()}]: ${format(msg.message.formatString, ...msg.message.args)}\n`);
-                });
-            }
+            // if (event.firstFive && event.level > LogLevel.info) {
+            //     output.write(`\t${LogLevel[event.level].toUpperCase()}: Printing the first and last 5 log message to aid in issue reproduction:\n`);
+            //     event.firstFive.forEach(msg => {
+            //         output.write(`\tLOG #${msg.logCount}: [${LogLevel[msg.level].toUpperCase()}]: ${format(msg.message.formatString, ...msg.message.args)}\n`);
+            //     });
+            // }
+            // if (event.lastFive && event.level > LogLevel.info) {
+            //     if (event.logCount > 11)
+            //         output.write(`\t[... ${event.logCount - 11} more messages ...]\n`);
+            //     event.lastFive.forEach(msg => {
+            //         output.write(`\tLOG #${msg.logCount}: [${LogLevel[msg.level].toUpperCase()}]: ${format(msg.message.formatString, ...msg.message.args)}\n`);
+            //     });
+            // }
         }
     }
 }
